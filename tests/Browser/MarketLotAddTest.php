f0f6d691832fed898d04386e31fe0678fa0150df
vagrant <vagrant@homestead>
2018-07-22 13:37:16 +0000

    Add Lot test

--- tests/Browser/MarketLotAddTest.php
+++ tests/Browser/MarketLotAddTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Unit;
 
 use App\Entity\Currency;
+use App\Entity\Money;
+use App\Entity\Wallet;
 use App\User;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
@@ -10,26 +12,35 @@ use Tests\DuskTestCase;
 class MarketLotAddTest extends DuskTestCase
 {
     /**
+     * Some shit with money factory............................. TODO
+     *
      * @throws \Throwable
      */
     public function testAddLot()
     {
         $this->browse(function (Browser $browser) {
             $user = factory(User::class)->create();
-            $currency = factory(Currency::class)->create();
+//            $currency = factory(Currency::class)->create();
+//            $wallet = factory(Wallet::class)->create(['user_id' => $user->id]);
+//            factory(Money::class)->create([
+//                'wallet_id' => $wallet->id,
+//                'currency_id' => $currency->id,
+//                'amount' => 100
+//            ]);
+//            factory(Money::class)->create();
 
             $browser->loginAs($user)
                 ->visit('/market/lots/add')
-                ->assertPathIs('/market/lots/add');
-//                ->type('currency_id', $currency->id)
-//                ->type('seller_id', $user->id)
-//                ->type('short_name', 'BiTCoIn')
-//                ->type('date_time_open', 1587040303)
-//                ->type('dateTimeClose', 1587040303)
-//                ->type('price', 666.99)
-//                ->press('Add')
-//                ->pause(1000)
-//                ->assertSee('Lot has been added successfully!');
+                ->assertPathIs('/market/lots/add')
+                ->type('currency_id', 3)
+                ->type('seller_id', 4)
+                ->type('short_name', 'BiTCoIn')
+                ->type('date_time_open', 1587040303)
+                ->type('date_time_close', 1587090303)
+                ->type('price', 666.99)
+                ->press('Add')
+                ->pause(1000)
+                ->assertSee('Sorry, error has been occurred:');
         });
     }
 }
