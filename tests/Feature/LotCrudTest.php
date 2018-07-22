6d3e1a92813c60a4705da907c18aa2ebe08a1aa0
vagrant <vagrant@homestead>
2018-07-22 14:44:07 +0000

    Functional tests

--- tests/Feature/LotCrudTest.php
+++ tests/Feature/LotCrudTest.php
@@ -3,9 +3,9 @@
 namespace Tests\Unit;
 
 use App\Entity\Currency;
+use App\Entity\Lot;
 use App\Entity\Money;
 use App\Entity\Wallet;
-use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use App\User;
@@ -28,11 +28,21 @@ class LotCrudTest extends TestCase
         $response = $this->actingAs($user)->json('POST', '/api/v1/lots', [
             "currency_id" => $currency->id,
             'seller_id' => $user->id,
-            "date_time_open" => Carbon::now()->toDateTimeString(),
-            "date_time_close" => Carbon::tomorrow()->toDateTimeString(),
+            "date_time_open" => strtotime('-1day'),
+            "date_time_close" => strtotime('+2day'),
             "price" => 666.99
         ]);
 //        dump($response);
         $response->assertStatus(201);
+    }
+
+    public function testGetLot()
+    {
+        $lot = factory(Lot::class)->create();
+        $response = $this->json('GET', '/api/v1/lots/'. $lot->id);
+        $lotResponse = json_decode($response->content(), true);
+
+        $response->assertStatus(200);
+        $this->assertEquals($lot->price, $lotResponse['price']);
     }
 }
